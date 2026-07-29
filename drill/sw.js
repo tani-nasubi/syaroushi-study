cab5ba822cdd3b9c343c2a438b391266f864df84
Tani Yutaro <nasubi.tani@gmail.com>
2026-07-29 21:18:42 +0900

    資料を移動して戻ったとき、読んでいた位置に復帰させる
    深掘りページから戻ると常に先頭に飛んでいた。showNote() が描画の最後で
    無条件に window.scrollTo(0,0) していたため。
    
    画面ごとのスクロール位置を控えておき、離れる前に保存・戻ったら復元する。
    ブラウザ標準の復元は再描画に間に合わず先頭に戻るので manual にした。
    
    - 見出し指定つきのリンク（#見出し）はアンカーへの移動を優先する
    - 出題中・模試中の画面は毎回先頭から始める

--- drill/sw.js
+++ drill/sw.js
@@ -7,7 +7,7 @@
  *    表示は即座に、次に開いたときには最新になる
  *  - バージョンが変わったら古いキャッシュを消す
  */
-const VERSION = "1fb2ae45c246";
+const VERSION = "c7cf90c8ee05";
 const CACHE   = "sharoushi-" + VERSION;
 
 self.addEventListener("install", () => self.skipWaiting());
